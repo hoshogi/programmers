b2dec5b1f6e8f6636909e33244212c98be0c5bb6
hoshogi <hoshogi@gmail.com>
2022-06-20 17:30:50 +0900

    Level1: 문자열을 정수로 바꾸기

---
+++ Level1/문자열을_정수로_바꾸기.cpp
@@ -0,0 +1,16 @@
+/*
+Title   : 문자열을 정수로 바꾸기
+Author  : Hoseok Lee
+Date    : 2022/06/20
+https://programmers.co.kr/learn/courses/30/lessons/12925
+https://github.com/hoshogi
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    return stoi(s);
+}
