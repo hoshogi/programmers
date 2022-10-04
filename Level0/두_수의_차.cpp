4cb0d75546361c75a38b79b5a8d5f29f20e016ed
hoshogi <hoshogi@gmail.com>
2022-10-04 18:50:16 +0900

    Level0: 두 수의 차

---
+++ Level0/두_수의_차.cpp
@@ -0,0 +1,16 @@
+/*
+Title   : 두 수의 차
+Author  : Hoseok Lee
+Date    : 2022/10/04
+https://school.programmers.co.kr/learn/courses/30/lessons/120803
+https://github.com/hoshogi
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    return num1 - num2;
+}
