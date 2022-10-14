9ca8be4a84cd3046d89d5c33d69b388ecf5acc13
hoshogi <hoshogi@gmail.com>
2022-10-14 21:38:51 +0900

    Level1: 이름이 있는 동물의 아이디

---
+++ Level1/이름이_있는_동물의_아이디.sql
@@ -0,0 +1,11 @@
+/*
+Title   : 이름이 있는 동물의 아이디
+Author  : Hoseok Lee
+Date    : 2022/10/14
+https://school.programmers.co.kr/learn/courses/30/lessons/59407
+https://github.com/hoshogi
+*/
+
+SELECT ANIMAL_ID 
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL;
