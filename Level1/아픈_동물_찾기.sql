005bad5cdfbbf78176acb7047898448fa60747bb
hoshogi <hoshogi@gmail.com>
2022-10-14 17:12:31 +0900

    Level1: 아픈 동물 찾기

---
+++ Level1/아픈_동물_찾기.sql
@@ -0,0 +1,12 @@
+@@ -0,0 +1,39 @@
+/*
+Title   : 아픈 동물 찾기
+Author  : Hoseok Lee
+Date    : 2022/10/14
+https://school.programmers.co.kr/learn/courses/30/lessons/59036
+https://github.com/hoshogi
+*/
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick';
