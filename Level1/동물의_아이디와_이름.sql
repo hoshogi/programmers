d761347f495fc56be92cdf7b7931c32642095fd2
hoshogi <hoshogi@gmail.com>
2022-10-14 17:12:03 +0900

    Level1: 동물의 아이디와 이름

---
+++ Level1/동물의_아이디와_이름.sql
@@ -0,0 +1,10 @@
+/*
+Title   : 동물의 아이디와 이름
+Author  : Hoseok Lee
+Date    : 2022/10/14
+https://school.programmers.co.kr/learn/courses/30/lessons/59403
+https://github.com/hoshogi
+*/
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS;
