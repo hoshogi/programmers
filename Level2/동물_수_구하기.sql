f29e64c0e23827962b9172b8f79cba020a130147
hoshogi <hoshogi@gmail.com>
2022-10-15 22:07:36 +0900

    Level2: 동물 수 구하기

---
+++ Level2/동물_수_구하기.sql
@@ -0,0 +1,10 @@
+/*
+Title   : 동물 수 구하기
+Author  : Hoseok Lee
+Date    : 2022/10/14
+https://school.programmers.co.kr/learn/courses/30/lessons/59406
+https://github.com/hoshogi
+*/
+
+SELECT COUNT(*)
+FROM ANIMAL_INS;
