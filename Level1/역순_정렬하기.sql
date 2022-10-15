3fd51f0ab5714158740f48e8fbd8c442c11829d7
hoshogi <hoshogi@gmail.com>
2022-10-15 21:37:22 +0900

    Level1: 역순 정렬하기

---
+++ Level1/역순_정렬하기.sql
@@ -0,0 +1,11 @@
+/*
+Title   : 역순 정렬하기
+Author  : Hoseok Lee
+Date    : 2022/10/15
+https://school.programmers.co.kr/learn/courses/30/lessons/59035
+https://github.com/hoshogi
+*/
+
+SELECT NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID DESC;
