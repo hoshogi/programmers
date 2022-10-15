ff4349867d44420aaf67dc879715a26d44d05c49
hoshogi <hoshogi@gmail.com>
2022-10-15 22:44:43 +0900

    Level2: 최솟값 구하기

---
+++ Level2/최솟값_구하기.sql
@@ -0,0 +1,10 @@
+/*
+Title   : 최솟값 구하기
+Author  : Hoseok Lee
+Date    : 2022/10/14
+https://school.programmers.co.kr/learn/courses/30/lessons/59038
+https://github.com/hoshogi
+*/
+
+SELECT MIN(DATETIME)
+FROM ANIMAL_INS;
