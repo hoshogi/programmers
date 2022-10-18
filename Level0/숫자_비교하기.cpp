2e2976fddb54bd2a7ae760d5035fa45ea6d0a590
hoshogi <hoshogi@gmail.com>
2022-10-18 15:11:33 +0900

    Level0: 숫자 비교하기

---
+++ Level0/숫자_비교하기.cpp
@@ -0,0 +1,16 @@
+/*
+Title   : 숫자 비교하기
+Author  : Hoseok Lee
+Date    : 2022/10/18
+https://school.programmers.co.kr/learn/courses/30/lessons/120807
+https://github.com/hoshogi
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    return num1 == num2 ? 1 : -1;
+}
