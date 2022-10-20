01e2387f5b5e907a87ca90d34c491ede51ed3c04
hoshogi <hoshogi@gmail.com>
2022-10-20 14:35:18 +0900

    Level0: 나머지 구하기

---
+++ Level0/나머지_구하기.cpp
@@ -0,0 +1,16 @@
+/*
+Title   : 나머지 구하기
+Author  : Hoseok Lee
+Date    : 2022/10/20
+https://school.programmers.co.kr/learn/courses/30/lessons/120810
+https://github.com/hoshogi
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    return num1 % num2;
+}
