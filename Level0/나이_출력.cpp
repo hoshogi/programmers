32b76a19e06df035d5957f5ec8bf277886c95b51
hoshogi <hoshogi@gmail.com>
2022-10-23 12:32:03 +0900

    Level0: 나이 출력

---
+++ Level0/나이_출력.cpp
@@ -0,0 +1,16 @@
+/*
+Title   : 나이 출력
+Author  : Hoseok Lee
+Date    : 2022/10/23
+https://school.programmers.co.kr/learn/courses/30/lessons/120820
+https://github.com/hoshogi
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int age) {
+    return 2023 - age;
+}
