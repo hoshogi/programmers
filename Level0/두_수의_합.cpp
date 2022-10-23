30ce40d9ef74603bbc9113469611a79eb7442a7f
hoshogi <hoshogi@gmail.com>
2022-10-23 12:30:06 +0900

    Level0: 두 수의 합

---
+++ Level0/두_수의_합.cpp
@@ -0,0 +1,16 @@
+/*
+Title   : 두 수의 합
+Author  : Hoseok Lee
+Date    : 2022/10/23
+https://school.programmers.co.kr/learn/courses/30/lessons/120802
+https://github.com/hoshogi
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    return num1 + num2;
+}
