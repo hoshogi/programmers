1ec17952a77aa71cc2cdbc46ff3591a84a39280d
hoshogi <hoshogi@gmail.com>
2022-10-26 12:13:02 +0900

    Level0: 두 수의 나눗셈

---
+++ Level0/두_수의_나눗셈.cpp
@@ -0,0 +1,16 @@
+/*
+Title   : 두 수의 나눗셈
+Author  : Hoseok Lee
+Date    : 2022/10/26
+https://school.programmers.co.kr/learn/courses/30/lessons/120806
+https://github.com/hoshogi
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    return (double)num1 / num2 * 1000;
+}
