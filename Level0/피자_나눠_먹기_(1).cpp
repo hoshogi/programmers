623ca4b4009dd6a80ed5a17a0a47046a177c6b46
hoshogi <hoshogi@gmail.com>
2022-10-29 00:00:38 +0900

    Level0: 피자 나눠 먹기 (1)

---
+++ Level0/피자_나눠_먹기_(1).cpp
@@ -0,0 +1,17 @@
+/*
+Title   : 피자 나눠 먹기 (1)
+Author  : Hoseok Lee
+Date    : 2022/10/28
+https://school.programmers.co.kr/learn/courses/30/lessons/120814
+https://github.com/hoshogi
+*/
+
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n) {
+    return ceil((double)n / 7);
+}
