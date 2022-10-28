7805b854baa92c60bcd6d92999a644b8a9ea91aa
hoshogi <hoshogi@gmail.com>
2022-10-28 13:48:08 +0900

    Level0: 편지

---
+++ Level0/편지.cpp
@@ -0,0 +1,16 @@
+/*
+Title   : 편지
+Author  : Hoseok Lee
+Date    : 2022/10/28
+https://school.programmers.co.kr/learn/courses/30/lessons/120898
+https://github.com/hoshogi
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string message) {
+    return 2 * message.length();
+}
