cd6b23ccb6868b55ad81590d1548f9527642bc39
hoshogi <hoshogi@gmail.com>
2023-04-21 21:12:07 +0900

    Level0: 문자열을 정수로 변환하기

---
+++ Level0/문자열을_정수로_변환하기.java
@@ -0,0 +1,13 @@
+/*
+Title   : 문자열을 정수로 변환하기
+Author  : Hoseok Lee
+Date    : 2023/04/21
+https://school.programmers.co.kr/learn/courses/30/lessons/181848
+https://github.com/hoshogi
+*/
+
+class Solution {
+    public int solution(String n_str) {
+        return Integer.parseInt(n_str);
+    }
+}
