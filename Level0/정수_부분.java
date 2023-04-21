0e6de3ee41a062536a30f73aeb331de3a1be2ea3
hoshogi <hoshogi@gmail.com>
2023-04-21 22:04:19 +0900

    Level0: 정수 부분

---
+++ Level0/정수_부분.java
@@ -0,0 +1,13 @@
+/*
+Title   : 정수 부분
+Author  : Hoseok Lee
+Date    : 2023/04/21
+https://school.programmers.co.kr/learn/courses/30/lessons/181850
+https://github.com/hoshogi
+*/
+
+class Solution {
+    public int solution(double flo) {
+        return (int)flo;
+    }
+}
