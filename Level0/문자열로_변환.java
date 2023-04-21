a99a327b9c7ae3d3d63380a10dd368b98e8ca7ef
hoshogi <hoshogi@gmail.com>
2023-04-21 22:05:20 +0900

    Level0: 문자열로 변환

---
+++ Level0/문자열로_변환.java
@@ -0,0 +1,13 @@
+/*
+Title   : 문자열로 변환
+Author  : Hoseok Lee
+Date    : 2023/04/21
+https://school.programmers.co.kr/learn/courses/30/lessons/181845
+https://github.com/hoshogi
+*/
+
+class Solution {
+    public String solution(int n) {
+        return Integer.toString(n);
+    }
+}
