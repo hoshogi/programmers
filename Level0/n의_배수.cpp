3c95677afeccf1c376a0d5fc258b6e463f4bf590
hoshogi <hoshogi@gmail.com>
2023-04-21 21:56:27 +0900

    Level0: n의 배수

---
+++ Level0/n의_배수.cpp
@@ -0,0 +1,15 @@
+/*
+Title   : n의 배수
+Author  : Hoseok Lee
+Date    : 2023/04/21
+https://school.programmers.co.kr/learn/courses/30/lessons/181937
+https://github.com/hoshogi
+*/
+
+class Solution {
+    public int solution(int num, int n) {
+        if (num % n == 0)
+            return 1;
+        return 0;
+    }
+}
