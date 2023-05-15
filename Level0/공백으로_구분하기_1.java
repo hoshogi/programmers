f35efa32e50b6c6b56c453c65c00c12c5712b79c
hoshogi <hoshogi@gmail.com>
2023-05-15 18:59:47 +0900

    Level0: 공백으로 구분하기 1

---
+++ Level0/공백으로_구분하기_1.java
@@ -0,0 +1,13 @@
+/*
+Title   : 공백으로 구분하기 1
+Author  : Hoseok Lee
+Date    : 2023/05/15
+https://school.programmers.co.kr/learn/courses/30/lessons/181869
+https://github.com/hoshogi
+*/
+
+class Solution {
+    public String[] solution(String my_string) {
+        return my_string.split(" ");
+    }
+}
