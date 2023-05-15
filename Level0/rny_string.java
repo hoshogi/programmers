a62fad519575ecc2450a99203dc828d214f8717f
hoshogi <hoshogi@gmail.com>
2023-05-15 13:55:02 +0900

    Level0: rny_string

---
+++ Level0/rny_string.java
@@ -0,0 +1,13 @@
+/*
+Title   : rny_string
+Author  : Hoseok Lee
+Date    : 2023/05/15
+https://school.programmers.co.kr/learn/courses/30/lessons/181863
+https://github.com/hoshogi
+*/
+
+class Solution {
+    public String solution(String rny_string) {
+        return rny_string.replace("m", "rn");
+    }
+}
