9a499898bc280aae984581555d34ebf8a8808356
hoshogi <hoshogi@gmail.com>
2023-05-15 10:40:14 +0900

    Level0: 대문자로 바꾸기

---
+++ Level0/대문자로_바꾸기.java
@@ -0,0 +1,13 @@
+/*
+Title   : 대문자로 바꾸기
+Author  : Hoseok Lee
+Date    : 2023/05/15
+https://school.programmers.co.kr/learn/courses/30/lessons/181877
+https://github.com/hoshogi
+*/
+
+class Solution {
+    public String solution(String myString) {
+        return myString.toUpperCase();
+    }
+}
