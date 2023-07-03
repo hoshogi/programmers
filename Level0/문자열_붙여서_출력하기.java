b783e071d077dcff5963dcab3bb0c17f03b89906
hoshogi <hoshogi@gmail.com>
2023-07-03 18:35:02 +0900

    Level0: 문자열 붙여서 출력하기

---
+++ Level0/문자열_붙여서_출력하기.java
@@ -0,0 +1,19 @@
+/*
+Title   : 문자열 붙여서 출력하기
+Author  : Hoseok Lee
+Date    : 2023/07/03
+https://school.programmers.co.kr/learn/courses/30/lessons/181946
+https://github.com/hoshogi
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        
+        System.out.println(a + b);
+    }
+}
